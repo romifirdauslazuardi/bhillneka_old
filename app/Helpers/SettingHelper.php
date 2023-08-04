6fb136f4a8106175dcc64f9a6cb222102caad071
yudo23 <yudo.dendy23@gmail.com>
2023-08-05 01:38:56 +0700

    Revisi fix bug

--- app/Helpers/SettingHelper.php
+++ app/Helpers/SettingHelper.php
@@ -126,7 +126,7 @@ class SettingHelper
                             $customer_total_fee = round($customer_total_fee);                  
                         }
                         else{
-                            Log::info($order->value);;
+                            Log::info($order);;
                             $customer_total_fee = $order->value;
                         }
 
