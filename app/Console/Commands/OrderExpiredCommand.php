c75ec5c57455db0f1ffc409d4c32568d0c6f7aed
yudo23 <yudo.dendy23@gmail.com>
2023-08-07 13:40:19 +0700

    Revisi notifikasi jatuh tempo

--- app/Console/Commands/OrderExpiredCommand.php
+++ app/Console/Commands/OrderExpiredCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Enums\OrderEnum;
 use App\Enums\ProductEnum;
 use App\Enums\ProductStockEnum;
+use App\Helpers\WhatsappHelper;
 use Illuminate\Console\Command;
 use App\Models\Order;
 use App\Models\ProductStock;
@@ -36,43 +37,42 @@ class OrderExpiredCommand extends Command
         DB::beginTransaction();
         try {
             $orders = new Order();
-            $orders = $orders->where("status",OrderEnum::STATUS_WAITING_PAYMENT);
+            $orders = $orders->where("status", OrderEnum::STATUS_WAITING_PAYMENT);
             $orders = $orders->get();
 
-            foreach($orders as $index => $row){
-                if(strtotime($row->expired_date) < strtotime(date("Y-m-d H:i:s"))){
+            foreach ($orders as $index => $row) {
+                if (strtotime($row->expired_date) < strtotime(date("Y-m-d H:i:s"))) {
                     $row->update([
                         'status' => OrderEnum::STATUS_EXPIRED,
                         'progress' => OrderEnum::PROGRESS_EXPIRED,
                     ]);
 
-                    foreach($row->items as $i => $orderItem){
-                        if($orderItem->product->is_using_stock == ProductEnum::IS_USING_STOCK_TRUE){
-                            $available = $orderItem->product->stocks()->where("type",ProductStockEnum::TYPE_MASUK)->sum("qty") - $orderItem->product->stocks()->where("type",ProductStockEnum::TYPE_KELUAR)->sum("qty");
-    
+                    foreach ($row->items as $i => $orderItem) {
+                        if ($orderItem->product->is_using_stock == ProductEnum::IS_USING_STOCK_TRUE) {
+                            $available = $orderItem->product->stocks()->where("type", ProductStockEnum::TYPE_MASUK)->sum("qty") - $orderItem->product->stocks()->where("type", ProductStockEnum::TYPE_KELUAR)->sum("qty");
+
                             ProductStock::create([
                                 'date' => date("Y-m-d"),
                                 'type' => ProductStockEnum::TYPE_MASUK,
                                 'product_id' => $orderItem->product_id,
                                 'qty' => $orderItem->qty,
                                 'available' => $available + $orderItem->qty,
-                                'note' => 'Stok masuk order #'.$row->code,
+                                'note' => 'Stok masuk order #' . $row->code,
                             ]);
                         }
                     }
+
+                    WhatsappHelper::sendWhatsappOrderTemplate($row->id, "pesanan");
                 }
             }
 
             DB::commit();
 
             return CommandAlias::SUCCESS;
-
         } catch (\Throwable $th) {
             DB::rollBack();
             Log::emergency($th->getMessage());
             return CommandAlias::FAILURE;
         }
-
-        
     }
 }
